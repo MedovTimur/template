f13c81b9ef3bceabeb0702efc5018b696baf16e8
MedovTimur <medov.tr@phystech.edu>
2024-07-29 11:41:47 +0300

    rename gwasm-builder to gear-wasm-builder

--- wasm/build.rs
+++ wasm/build.rs
@@ -3,7 +3,7 @@ use sails_idl_gen::program;
 use std::{env, path::PathBuf};
 
 fn main() {
-    gwasm_builder::build();
+    gear_wasm_builder::build();
 
     program::generate_idl_to_file::<Program>(
         PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("{{project-name}}.idl"),
